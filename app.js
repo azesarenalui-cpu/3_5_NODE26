be75e6d8eab8530afc26f5b1b8e147fe694c251e
NieNyel <joru.cruz.ui@phinmaed.com>
2026-09-11 12:22:36 +0800

    adsada

--- app.js
+++ app.js
@@ -8,26 +8,27 @@ const port = 9000;
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-const db =mysql.createConnection({
-    host: 'localhost',
-    user: "root",
-    password:"",
-    database: "ui_3_3_26"
-});
-
-
 
 app.get('/', (req, res) => {
     res.render(`index`);
 })
-
 
 app.post('/register', (req, res) => {
     const lastname =req.body.lastname;
     const firstname = req.body.firstname;
     const age = req.body.age;
     res.render('content', { lastname, firstname, age });
+
+    const insert = `INSERT INTO tbl_students VALUES('0','${lastname}','${firstname}','${age}')`;
+
+    conn.query(insert, (err, result) => {
+        if(err) throw err;
+        console.log("data inserted");
+        res.send(`<script>
+            alert('data inserted'); 
+            location.href='/'; </script>`);
+        });
 });
 app.listen(9000, () => {
-    console.log("server is running on port");
+    console.log("server is running on port 9000");
 });
